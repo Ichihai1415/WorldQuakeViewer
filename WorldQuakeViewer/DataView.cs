fa182a55d6a9f6604f4ad86db7049c8fd3504367
Ichihai1415 <projects31415.gif@gmail.com>
2023-10-09 21:56:22 +0900

    <設定内容だけ先に作るのでここで一時停止>ちょっと追加

--- WorldQuakeViewer/DataView.cs
+++ WorldQuakeViewer/DataView.cs
@@ -1,17 +1,41 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace WorldQuakeViewer
 {
     public partial class DataView : Form
     {
-        readonly ViewData dataSource = ViewData.Null;
+        readonly ViewData vd = ViewData.Null;
 
-        public DataView(ViewData ds)
+        public DataView(ViewData viewData)
         {
-            dataSource = ds;
             InitializeComponent();
-            Text = $"WorldQuakeViewer - {dataSource}";
+
+            vd = viewData;
+            Text = $"WorldQuakeViewer - {vd}";
+            switch((int)vd%10)
+            {
+                case 1:
+                    ClientSize = new Size(400, 100);
+                    break;
+                case 2:
+                    ClientSize = new Size(400, 500);
+                    break;
+                case 3:
+                    ClientSize = new Size(800, 400);
+                    break;
+            }
+
+
+
+
+
+
+
+
+
+
         }
 
         private void DataView_Load(object sender, EventArgs e)
