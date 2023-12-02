2ee44a2c2349455c72332f7ce35f82c64f0e8762
Ichihai1415 <projects31415.gif@gmail.com>
2023-12-02 12:15:50 +0900

    まとめる

--- WorldQuakeViewer/ControlForm.Designer.cs
+++ WorldQuakeViewer/ControlForm.Designer.cs
@@ -36,17 +36,19 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.DGV_pro = new System.Windows.Forms.DataGridView();
+            this.GDV_pro_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GDV_pro_USGS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GDV_pro_EMSC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GDV_pro_EarlyEst = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GDV_pro_GFZ = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tabPage6 = new System.Windows.Forms.TabPage();
-            this.tabPage10 = new System.Windows.Forms.TabPage();
-            this.tabPage9 = new System.Windows.Forms.TabPage();
-            this.tabPage8 = new System.Windows.Forms.TabPage();
-            this.tabControl2 = new System.Windows.Forms.TabControl();
             this.TabCtrl_Main.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage3.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage5.SuspendLayout();
-            this.tabControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_pro)).BeginInit();
             this.SuspendLayout();
             // 
             // TabCtrl_Main
@@ -84,10 +86,10 @@
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 27);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(492, 469);
+            this.tabPage2.Size = new System.Drawing.Size(492, 474);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "情報";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -125,7 +127,7 @@
             // 
             // tabPage5
             // 
-            this.tabPage5.Controls.Add(this.tabControl2);
+            this.tabPage5.Controls.Add(this.DGV_pro);
             this.tabPage5.Location = new System.Drawing.Point(4, 27);
             this.tabPage5.Name = "tabPage5";
             this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
@@ -134,55 +136,60 @@
             this.tabPage5.Text = "処理";
             this.tabPage5.UseVisualStyleBackColor = true;
             // 
+            // DGV_pro
+            // 
+            this.DGV_pro.AllowUserToAddRows = false;
+            this.DGV_pro.AllowUserToDeleteRows = false;
+            this.DGV_pro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_pro.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.GDV_pro_Name,
+            this.GDV_pro_USGS,
+            this.GDV_pro_EMSC,
+            this.GDV_pro_EarlyEst,
+            this.GDV_pro_GFZ});
+            this.DGV_pro.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DGV_pro.Location = new System.Drawing.Point(3, 3);
+            this.DGV_pro.Name = "DGV_pro";
+            this.DGV_pro.RowHeadersVisible = false;
+            this.DGV_pro.RowTemplate.Height = 21;
+            this.DGV_pro.Size = new System.Drawing.Size(478, 432);
+            this.DGV_pro.TabIndex = 1;
+            // 
+            // GDV_pro_Name
+            // 
+            this.GDV_pro_Name.HeaderText = "項目";
+            this.GDV_pro_Name.Name = "GDV_pro_Name";
+            this.GDV_pro_Name.ReadOnly = true;
+            // 
+            // GDV_pro_USGS
+            // 
+            this.GDV_pro_USGS.HeaderText = "USGS";
+            this.GDV_pro_USGS.Name = "GDV_pro_USGS";
+            this.GDV_pro_USGS.ReadOnly = true;
+            // 
+            // GDV_pro_EMSC
+            // 
+            this.GDV_pro_EMSC.HeaderText = "EMSC";
+            this.GDV_pro_EMSC.Name = "GDV_pro_EMSC";
+            // 
+            // GDV_pro_EarlyEst
+            // 
+            this.GDV_pro_EarlyEst.HeaderText = "Early-est";
+            this.GDV_pro_EarlyEst.Name = "GDV_pro_EarlyEst";
+            // 
+            // GDV_pro_GFZ
+            // 
+            this.GDV_pro_GFZ.HeaderText = "GFZ";
+            this.GDV_pro_GFZ.Name = "GDV_pro_GFZ";
+            // 
             // tabPage6
             // 
-            this.tabPage6.Location = new System.Drawing.Point(4, 27);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
             this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Size = new System.Drawing.Size(484, 438);
+            this.tabPage6.Size = new System.Drawing.Size(484, 443);
             this.tabPage6.TabIndex = 2;
             this.tabPage6.Text = "表示";
             this.tabPage6.UseVisualStyleBackColor = true;
-            // 
-            // tabPage10
-            // 
-            this.tabPage10.Location = new System.Drawing.Point(4, 27);
-            this.tabPage10.Name = "tabPage10";
-            this.tabPage10.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage10.Size = new System.Drawing.Size(476, 407);
-            this.tabPage10.TabIndex = 3;
-            this.tabPage10.Text = "GFZ";
-            this.tabPage10.UseVisualStyleBackColor = true;
-            // 
-            // tabPage9
-            // 
-            this.tabPage9.Location = new System.Drawing.Point(4, 27);
-            this.tabPage9.Name = "tabPage9";
-            this.tabPage9.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage9.Size = new System.Drawing.Size(476, 407);
-            this.tabPage9.TabIndex = 2;
-            this.tabPage9.Text = "Early-est";
-            this.tabPage9.UseVisualStyleBackColor = true;
-            // 
-            // tabPage8
-            // 
-            this.tabPage8.Location = new System.Drawing.Point(4, 27);
-            this.tabPage8.Name = "tabPage8";
-            this.tabPage8.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage8.Size = new System.Drawing.Size(476, 407);
-            this.tabPage8.TabIndex = 1;
-            this.tabPage8.Text = "EMSC";
-            this.tabPage8.UseVisualStyleBackColor = true;
-            // 
-            // tabControl2
-            // 
-            this.tabControl2.Controls.Add(this.tabPage8);
-            this.tabControl2.Controls.Add(this.tabPage9);
-            this.tabControl2.Controls.Add(this.tabPage10);
-            this.tabControl2.Location = new System.Drawing.Point(0, 0);
-            this.tabControl2.Name = "tabControl2";
-            this.tabControl2.SelectedIndex = 0;
-            this.tabControl2.Size = new System.Drawing.Size(484, 438);
-            this.tabControl2.TabIndex = 0;
             // 
             // CtrlForm
             // 
@@ -199,7 +206,7 @@
             this.tabPage3.ResumeLayout(false);
             this.tabControl1.ResumeLayout(false);
             this.tabPage5.ResumeLayout(false);
-            this.tabControl2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_pro)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -215,9 +222,11 @@
         private System.Windows.Forms.TabPage tabPage4;
         private System.Windows.Forms.TabPage tabPage5;
         private System.Windows.Forms.TabPage tabPage6;
-        private System.Windows.Forms.TabControl tabControl2;
-        private System.Windows.Forms.TabPage tabPage8;
-        private System.Windows.Forms.TabPage tabPage9;
-        private System.Windows.Forms.TabPage tabPage10;
+        private System.Windows.Forms.DataGridView DGV_pro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GDV_pro_Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GDV_pro_USGS;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GDV_pro_EMSC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GDV_pro_EarlyEst;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GDV_pro_GFZ;
     }
 }
